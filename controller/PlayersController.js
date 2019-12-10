81a0572a53e230d84a1a4b069ab60c612bdf2b6f
Turza006 <probaldattaturza@gmail.com>
2019-12-10 23:58:28 +0600

    google token added

--- controller/PlayersController.js
+++ controller/PlayersController.js
@@ -1,10 +1,10 @@
 const Players = require('../model/Players')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(google_auth.googleAuth.clientID);
+
 module.exports = {
     createPlayers: async (req, res) => {
         const body = req.body
-        const name = body.name
-        const email = body.email
-        const profile_pic = body.profile_pic
         const xp = body.xp
         const coin = body.coin
         const playing = body.playing
@@ -12,32 +12,60 @@ module.exports = {
         const dimond = body.dimond
         const score = body.score
 
-
-        const players = await Players.findOne({email: email})
-
-        if (players) {
-            return res.status(401).json({
-                msg: "Players Exist With The Email"
+        async function verify() {
+            const ticket = await client.verifyIdToken({
+                idToken: body.googleToken,
+                audience: ""
             })
+            const payload = ticket.getPayload();
+            console.log(payload)
+
+            if (payload.exp > Date.now() / 1000) {
+                const player = await Players.findOne({email: payload.email}, {
+                    firstName: 1,
+                    lastName: 1,
+                    fullName: 1,
+                    email: 1,
+                    userName: 1,
+                    _id: 1,
+                    profileImage: 1,
+                    loginType: 1,
+                })
+
+                const count = await Players.find().count()
+                const newPlayer = new Players();
+                newPlayer.firstName = payload.given_name
+                newPlayer.lastName = payload.family_name
+                newPlayer.fullName = payload.name
+                newPlayer.email = payload.email
+                newPlayer.profileImage = payload.picture
+                newPlayers.xp = xp
+                newPlayers.coin = coin
+                newPlayers.playing = playing
+                newPlayers.country = country
+                newPlayers.dimond = dimond
+                newPlayers.score = score
+                if (count > 0) {
+                    newPlayer.userName = player.firstName.toLowerCase() + "." + player.lastName.toLowerCase() + "." + count.toString()
+                } else {
+                    newPlayer.userName = player.firstName.toLowerCase() + "." + player.lastName.toLowerCase()
+                }
+                newPlayer.loginType = "Google"
+
+                await tokenCreator("Players", player._id)
+                await newPlayer.save()
+                res.status(201).json(newPlayer)
+            }
         }
 
-        const newPlayers = new Players()
-        if (email) {
-            newPlayers.name = name
-            newPlayers.email = email
-            newPlayers.profile_pic = profile_pic
-            newPlayers.xp = xp
-            newPlayers.coin = coin
-            newPlayers.playing = playing
-            newPlayers.country = country
-            newPlayers.dimond = dimond
-            newPlayers.score = score
-        }
-
-
-            await newPlayers.save()
-            res.status(201).json(newPlayers)
+        verify().catch(console.error);
     },
+
+
+
+
+
+
     singlePlayer: async (req,res) =>{
         const myData = await Players.findOne({email : req.params.email}).populate('Players')
         res.status(200).json(myData)
